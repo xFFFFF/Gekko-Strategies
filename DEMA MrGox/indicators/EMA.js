913dcac45c35d84fb9fdfaea0d756043697807de
xFFFFF <35547536+xFFFFF@users.noreply.github.com>
2018-02-18 23:33:58 +0100

    Add files via upload

---
+++ DEMA MrGox/indicators/EMA.js
@@ -0,0 +1,35 @@
+﻿var Indicator = function(weight) {
+  this.input = 'price';
+  this.weight = weight;
+  this.result = false;
+  this.age = 0;
+}
+
+Indicator.prototype.update = function(price) {
+  // 第一次进入，无法计算EMA值，因为没有yesterday
+  if(this.result === false)
+    this.result = price;
+
+  this.age++;
+  this.calculate(price);
+
+  return this.result;
+}
+
+//  同上方公式，
+//    calculation (based on tick/day):
+//  EMA = Price(t) * k + EMA(y) * (1 – k)
+//  t = today, y = yesterday, N = number of days in EMA, k = 2 / (N+1)
+Indicator.prototype.calculate = function(price) {
+  // weight factor
+  var k = 2 / (this.weight + 1);
+
+  // yesterday
+  var y = this.result;
+  
+  // calculation
+  this.result = price * k + y * (1 - k);
+}
+
+module.exports = Indicator;
+
